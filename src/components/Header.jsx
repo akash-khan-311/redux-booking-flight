279a91a66ed8cb49913bca2dc811107a5a87705f
Akash Khan <akashcc48@gmail.com>
2024-02-02 20:33:16 +0600

    header logo , background image and favicon changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ const Header = () => {
     <>
       <header id="header">
         <div className="container">
-          <img src="/src/assets/images/lws-logo.svg" alt="logo" className="logo" />
+         <div className="flex items-center gap-x-3">
+         <img src="/src/assets/images/me2.png"  alt="logo" className="w-16 h-16" />
+         <h2 className="text-orange-500 text-2xl font-bold italic">Akash Khan</h2>
+         </div>
           <div className="flex items-center">
             <a className="text-white min-w-[50px] font-medium" href="#">
               Home
