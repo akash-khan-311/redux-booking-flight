9041dc03eee7e4f13c13bd4fc7069bf362035069
Akash Khan <akashcc48@gmail.com>
2024-02-02 17:07:39 +0600

    redux action , actionTypes,reducer has created and front end ui created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import BookingSection from "./components/BookingSection";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+     <Header/>
+     <BookingSection/>
+    </>
+  );
+}
+
+export default App;
